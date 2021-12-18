0e4070507963c6054192bd8f8adb7bfe4ebcf451
Rafael Vieira <rafaelvieirasantos2006@hotmail.com>
2021-12-18 07:57:31 -0300

    Project created

---
+++ src/main/java/com/rafaelvieira/studentemail/StudentEmailApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelvieira.studentemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelvieira/studentemail/StudentEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelvieira.studentemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
